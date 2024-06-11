1569c5e52c13b860ce560833818d58607a23f168
jfCoronel <jfc@us.es>
2024-06-11 10:13:35 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # VisualOpenSimula
 Visual simple interface for OpenSimula, Python Streamlit
+
+_-- jfCoronel --
---
+++ projects.py
@@ -0,0 +1,6 @@
+import streamlit as st
+col1, col2 = st.columns(2)
+col2.image('img/icon_opensimula.svg',width=48)
+col1.write("### VisualOpenSimula")
+
+st.write('**Projects Editor**')
